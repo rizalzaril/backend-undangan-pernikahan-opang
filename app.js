304ad02a4c69496eb321347ac9908dd204876ab9
Rizal <zaril.ziral2020@gmail.com>
2024-11-13 15:48:42 +0700

    main

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use(
 );
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", allowedOrigin);
+  res.setHeader("Access-Control-Allow-Origin", origin);
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, PUT, DELETE, OPTIONS"
