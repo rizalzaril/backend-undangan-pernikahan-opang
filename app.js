df14517a07792664fce4a6aa3ac22830d53ba889
Rizal <zaril.ziral2020@gmail.com>
2024-11-13 22:41:38 +0700

    main

--- app.js
+++ app.js
@@ -30,9 +30,9 @@ const db = admin.firestore();
 
 // Cloudinary configuration
 cloudinary.v2.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: "djgr3hq5k",
+  api_key: "122714586646415",
+  api_secret: "utEKAi7kF1ExsyUxYtF7NJ_piRM",
 });
 
 // Multer configuration for file uploads
