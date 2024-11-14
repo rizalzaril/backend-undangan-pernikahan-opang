49f1d7625cf4dd435e5a03caf6471ad398e98d5b
Rizal <zaril.ziral2020@gmail.com>
2024-11-14 19:56:28 +0700

    main

--- app.js
+++ app.js
@@ -93,6 +93,16 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://web-wedding-invitation-umber.vercel.app"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 // Use the authentication middleware only for routes that require authentication
 app.use(bodyParser.json());
 
